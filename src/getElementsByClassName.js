d9d971f383e670150da7db83d2e6770ccf343918
alexminer88 <alex.miner@gmail.com>
2018-11-05 15:50:27 -0800

    Completed getElementsByClassName.js and stringifyJSON.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here123
+var getElementsByClassName = function(className) {
+  //start at body
+  // produce a list of elements that match className
+  //recursively go through body's elements and find if class matches className
+
+  var body = document.body;
+  var elements = [];
+      
+  function getElements(node) {
+    if(node.classList && node.classList.contains(className)) {
+      elements.push(node);
+    }
+    node.childNodes.forEach (function(el) {
+      getElements(el);
+    });
+  }
+
+  getElements(body);
+  return elements;
 };
+
+// if (className.childNodes) 
